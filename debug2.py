dfe5ca1d4481367e48bc884b6aeda9c850c61d2f
Nemo <nemo@server>
2026-05-31 01:04:58 +0900

    chore: 테스트 파일 정리 + .gitignore 패턴 추가

--- debug2.py
+++
@@ -1,7 +0,0 @@
-s = open("E:\\project\\full_page.html", encoding="utf-8").read()
-
-# Find content div
-idx = s.find('<div class="content">')
-print("Found content at:", idx)
-after = s[idx:]
-print(after[:800])
